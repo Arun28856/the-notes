0fc0458e9d7551d7c05e7bd32fcd1a125e9ffdfc
Admin <arunlakshman28@gmail.com>
2025-12-29 20:15:41 +0530

    Created a basic RESTful API using MongoDB as DB

--- backend/src/Controllers/notesController.js
+++ backend/src/Controllers/notesController.js
@@ -1,15 +1,45 @@
-export function getNotes(req, res) {
-    res.status(200).send("You have just fetched the notes");
+import note from "../models/note.js";
+
+export async function getNotes(req, res) {
+    try {
+        const notes = await note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error in getNotes:", error);
+        res.status(500).json({ message:"Internal Server Error"})
+    }
 }
 
-export function postNotes(req, res) {
-    res.status(200).send("You have just created the notes");
+export async function createNotes(req, res) {
+    try {
+        const {title, content} = req.body;
+        const newNote = new note({title, content});
+        await newNote.save();
+        res.status(201).json({message: "Note created successfully"});
+    } catch (error) {
+        console.error("Error while creating note:", error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
 
-export function putNotes(req, res) {
-    res.status(200).send("You have just updated the notes");
+export async function updateNotes(req, res) {
+    try {
+        const {title, content} = req.body;
+        await note.findByIdAndUpdate(req.params.id, {title, content});
+        res.status(200).json({message: "Note updated successfully"});
+    } catch (error) {
+        console.error("Error while updating note:", error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
 
-export function deleteNotes(req, res) {
-    res.status(200).send("You have just deleted the notes");
+export async function deleteNotes(req, res) {
+    try {
+        const {title, content} = req.body;
+        await note.findByIdAndDelete(req.params.id, {title, content});
+        res.status(200).json({message: "Note deleted succesfully"});
+    } catch (error) {
+        console.error("Error while deleting note:", error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
