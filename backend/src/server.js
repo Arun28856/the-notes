ae168059d2a46aaf4df4f121324d548c2ed17e08
Admin <arunlakshman28@gmail.com>
2025-12-30 15:37:31 +0530

    middleware and ratelimiting config

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from "express";
 import request from "./routes/restAPIs.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from './middleware/ratelimiter.js';
 
 console.log(process.env.MONGODB_URI);
 
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 connectDB();
 
 app.use(express.json());
-
+app.use(rateLimiter);
 app.use("/api/notes", request);
 
 app.listen(PORT, () => {
