5d54e510b6635e6e04ce30b4a587c495ed919a75
Admin <arunlakshman28@gmail.com>
2026-01-06 10:11:33 +0530

    Created Note Card component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,9 @@
 import Navbar  from '../components/navbar.jsx';
 import { useEffect, useState } from 'react';
 import Ratelimited from '../components/RatelimitedUI.jsx';
-import { data } from 'react-router';
+import NoteCard from '../components/NoteCard.jsx';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import note from '../../../backend/src/models/note.js';
 
 export const HomePage = () => {
   const [isRatelimited, setIsRatelimited] = useState(false);
@@ -38,15 +37,13 @@ export const HomePage = () => {
     <Navbar />
     {isRatelimited && <Ratelimited />}
     
-    <div className='max-w-7xl mx-auto p-4 mt-0'>
+    <div className='max-w-7xl mx-auto p-4 mt-8'>
       {loading && <div className='text-center text-secondary py-5'>Loading notes...</div>}
 
       {notes.length > 0 && !isRatelimited && (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           {notes.map(note => (
-            <div>
-              <NoteCard key={note._id} note={note} />
-            </div>
+            <NoteCard key={note._id} note={note} />
           ))}   
 
 
