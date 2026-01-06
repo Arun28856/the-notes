d1928f7703bdb05bbc791e84a139d77e6c62079c
Admin <arunlakshman28@gmail.com>
2026-01-06 09:03:59 +0530

    Created Nav bar component and rate limitting and enabled CORS

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,61 @@
-import React from 'react'
+import Navbar  from '../components/navbar.jsx';
+import { useEffect, useState } from 'react';
+import Ratelimited from '../components/RatelimitedUI.jsx';
+import { data } from 'react-router';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import note from '../../../backend/src/models/note.js';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isRatelimited, setIsRatelimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+
+      try {
+        const res = await axios.get("http://localhost:8080/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRatelimited(false);
+
+      }catch (error) {
+        console.log("Error fetching notes:");
+        if(error.response?.status === 429){
+          setIsRatelimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchNotes();
+  }, []);
+
+  return <div className = "min-h-screen">
+    <Navbar />
+    {isRatelimited && <Ratelimited />}
+    
+    <div className='max-w-7xl mx-auto p-4 mt-0'>
+      {loading && <div className='text-center text-secondary py-5'>Loading notes...</div>}
+
+      {notes.length > 0 && !isRatelimited && (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          {notes.map(note => (
+            <div>
+              <NoteCard key={note._id} note={note} />
+            </div>
+          ))}   
+
+
+        </div>
+      )}
+
+    </div>
+
+  </div>;
 }
+
+export default HomePage;
